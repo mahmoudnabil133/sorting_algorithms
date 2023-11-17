06a2dbd814a7f52d67d871229a271f6a03d9bae6
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-17 23:44:14 +0200

    Sorting_algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * swap - it swaps 2 elements of arr
+ * @a: first element
+ * @b: second element
+ * Return: no return value
+*/
+void swap(int *a, int *b)
+{int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * bubble_sort - it sorts an array with comparison and swaping
+ * @array: arr to be sorted
+ * @size: size of array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+	for (i = size - 1; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
