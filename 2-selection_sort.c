abdb0e21bf9d9653f4005709ddbfe730e386120d
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-18 11:04:10 +0200

    Sorting_algorithms

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,17 +23,14 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = array[i];
+		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
 		}
-		if (min_index != i)
-		{
-			swap(&array[min_index], &array[i]);
-			print_array(array, size);
-		}
+		swap(&array[min_index], &array[i]);
+		print_array(array, size);
 	}
 
 }
