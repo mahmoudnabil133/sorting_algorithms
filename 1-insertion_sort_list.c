1c6345a5d6c593cb03db6e51216cadf58669a02e
mahmoudnabil133 <hodanabil144@gmail.com>
2023-11-20 22:51:31 +0200

    Sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -36,7 +36,7 @@ void swap_head(listint_t *a, listint_t *b)
 void insertion_sort_list(listint_t **list)
 {listint_t *current, *next_itr;
 
-	if (!*list)
+	if (list == NULL || (*list)->next == NULL)
 		return;
 	current  = (*list)->next;
 	while (current != NULL)
